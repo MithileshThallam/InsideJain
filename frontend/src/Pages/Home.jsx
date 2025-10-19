a65f5f8b05620c0c9080a0f1b4a5b9fa8b77b7bd
mithilesh <mithileshthallam@gmail.com>
2025-10-19 20:29:32 +0530

    blog cards developed

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
+import Bloglist from '../Components/Bloglist'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Header />
+        <Bloglist/>
     </div>
   )
 }
